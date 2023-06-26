084f023e2f8b5b1c7ee0ed26328f1cff4509fe61
pie999 <105122549+pie999@users.noreply.github.com>
2023-06-26 15:49:58 +0200

    Add start script and delete unnecessary files

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const MessageSchema = new Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", MessageSchema);
